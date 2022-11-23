040ce0b55e9f508133af8bd035d1fe03762b83bc
Sebas <sbt.boisgard@gmail.com>
2022-11-23 13:24:51 +0000

    Exercise 1 - Create a Storyblok space and connect it with your Nuxt app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+				accessToken: '6Owjwu679yYSbGJJrRvvwwtt',
+				// If Server Location === US then:
+				// apiOptions: { region: 'us' }
+			},
+    ],
+    '@nuxtjs/tailwindcss'
 
+  ]
 })
